58e72d1048c411359c6e1395c1f764c373f1c30b
Deepthi <deepthisriya28@gmail.com>
2023-12-07 22:16:26 +0530

    fronted changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,138 +1,162 @@
 import React, { useState, useEffect } from 'react'
 import listServices from '../services/apiservice'
-import { Link } from '@mui/material'
-import { useParams ,Navigate, useNavigate} from 'react-router-dom';
+import { Button, Link } from '@mui/material'
+import { useParams, Navigate, useNavigate } from 'react-router-dom';
+import {
+  FormControl,
+  Input,
+  FormHelperText,
+  InputLabel,
+  TextField,
+  Box,
+  FilledInput,
+  InputAdornment
+} from '@mui/material'
+import { Label } from '@mui/icons-material';
 const Form = () => {
-    const navigate=useNavigate()
-    const [getList, setList] = useState([])
-    const [senddata, setSendData] = useState(false)
-    const [title, setTitle] = useState('')
-    const [comment, setComment] = useState('')
-    const [selectedItemId, setSelectedItemId] = useState(null);
-    const [error, setError] = useState(null);
+  const navigate = useNavigate()
+  const [getList, setList] = useState([])
+  const [senddata, setSendData] = useState(false)
+  const [title, setTitle] = useState('')
+  const [comment, setComment] = useState('')
+  const [selectedItemId, setSelectedItemId] = useState(null);
+  const [error, setError] = useState(null);
 
-    const apiResp = async () => {
-        const resp = await listServices.getApi()
+  const apiResp = async () => {
+    const resp = await listServices.getApi()
 
-        console.log(resp)
+    console.log(resp)
 
-        setList(resp?.data)
+    setList(resp?.data)
+  }
+  useEffect(() => {
+    apiResp()
+  }, [])
+  console.log("getlist", getList)
+
+  const { id } = useParams();
+  console.log("iddd", id)
+  const fetchData = async () => {
+    try {
+      if (id) {
+        const response = await listServices.getnotbyid(id);
+        // const { title, comment } = response.data;
+        console.log(response, "dataa")
+        setTitle(response.title)
+        setComment(response.comment)
+      }
+      else {
+        setTitle('')
+        setComment('')
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
-    useEffect(() => {
-        apiResp()
-    }, [])
-    console.log("getlist", getList)
+  };
+  useEffect(() => {
 
-    const { id } = useParams();
-    console.log("iddd",id)
-    const fetchData = async () => {
-        try {
-          if (id) {
-            const response = await listServices.getnotbyid(id);
-            // const { title, comment } = response.data;
-            console.log(response, "dataa")
-            setTitle(response.title)
-            setComment(response.comment)
-          }
-          else{
-            setTitle('')
-            setComment('')
-          }
-        } catch (error) {
-          console.error('Error fetching data:', error);
+
+    fetchData();
+  }, [id]);
+
+
+  const sendPostApi = async (e) => {
+    e.preventDefault()
+    // const dataobj={title,comment}
+
+    //         const d= await listServices.postApi(dataobj)
+    //         return d
+    //        console.log("succsfully savedd..",d)/
+    const dataobj = { title, comment };
+    if (dataobj.title !== '' && dataobj.comment !== '') {
+
+
+      try {
+        if (id) {
+          console.log(selectedItemId, "selectedItemId")
+          await listServices.updateApi(id, dataobj);
+          setSelectedItemId(null);
+
         }
-      };
-    useEffect(() => {
-     
-  
-      fetchData();
-    }, [id]);
-  
 
-    const sendPostApi = async (e) => {
-        e.preventDefault()
-        // const dataobj={title,comment}
-
-        //         const d= await listServices.postApi(dataobj)
-        //         return d
-        //        console.log("succsfully savedd..",d)/
-        const dataobj = { title, comment };
-        if (dataobj.title !== '' && dataobj.comment !== '') {
-
-
-            try {
-                if (id) {
-                    console.log(selectedItemId, "selectedItemId")
-                    await listServices.updateApi(id, dataobj);
-                    setSelectedItemId(null);
-                  
-                } 
-                
-                else {
-                    await listServices.postApi(dataobj);
-                }
-
-                apiResp();
-                setSendData(false);
-                setTitle('');
-                setComment('');
-                setError(null);
-                navigate('/list');
-            } catch (error) {
-                console.error("Error sending data:", error);
-                setError("Error sending data. Please try again.");
-            }
-        }
         else {
-            console.log("data cannot be empty")
+          await listServices.postApi(dataobj);
         }
-    }
-    // const updateItem = async (id) => {
-    //     try {
-    //         const selectedItem = getList.find((item) => item._id === id);
-    //         setTitle(selectedItem.title);
-    //         setComment(selectedItem.comment);
-    //         setSelectedItemId(id);
-    //         setSendData(true);
-    //         setError(null);
-    //     } catch (error) {
-    //         console.error("Error updating item:", error);
-    //         setError("Error updating item. Please try again.");
-    //     }
-    // };
 
-    
+        apiResp();
+        setSendData(false);
+        setTitle('');
+        setComment('');
+        setError(null);
+        navigate('/list');
+      } catch (error) {
+        console.error("Error sending data:", error);
+        setError("Error sending data. Please try again.");
+      }
+    }
+    else {
+      console.log("data cannot be empty")
+    }
+  }
+
+
+
   return (
     <div>
-       <div style={{ backgroundColor: '#e3fcea', padding: 30,  margin: "30px auto auto auto"
- ,width:"300px"  }}>
-    <h4 >{ id? "Update Note" :"Add Note"}</h4>
-                        <form >
-                            <input
-                                type='text'
-                                name='title'
-                                id='title'
-                                value={title}
-                                onChange={(e) => setTitle(e.target.value)}
-                                placeholder='title'
-                                
-                            />
-                            <br />
-                            <input
-                                type='text'
-                                name='comment'
-                                id='comment'
-                                value={comment}
-                                onChange={(e) => setComment(e.target.value)}
-                                placeholder='description'
-                            />
-                            <br />
-                            
-                            <button type='submit' style={{ margin: 10, padding: 5, backgroundColor:"#95a5ed" ,color:"white"}} onClick={sendPostApi}>{id ? "Update" :"Create"} </button>
-                           
-                        </form>
-                    </div>
+
+      <Box
+        sx={{
+          my: 8,
+          mx: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          width: '350px',
+          margin: ' 100px auto',
+          backgroundColor: '#b3d1ff',
+
+        }}
+
+      >
+        <h4 style={{ fontFamily: 'Arial, Helvetica, sans-serif', fontWeight: 'normal' }} >{id ? "Update Note" : "Add Note"}</h4>
+        <TextField
+          margin="normal"
+          // required
+          fullWidth
+          id="title"
+          label="title"
+          name="title"
+
+          sx={{ m: 1, width: '25ch' }}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder='title'
+          variant='filled'
+          style={{ backgroundColor: 'white', width: 'auto' }}
+
+        />
+
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="comment"
+          label="Description"
+          type="comment"
+          id="comment"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          placeholder='description'
+          variant='filled'
+          style={{ backgroundColor: 'white', width: 'auto' }}
+        />
+        <Button type='submit' style={{ margin: 15, padding: 8, backgroundColor: id ? "orange" : "#00b300", color: "white" }} onClick={sendPostApi}>{id ? "Update" : "Create"} </Button>
+      </Box>
     </div>
+
+
+
+
   )
 }
 
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import listServices from '../services/apiservice'
 import { useNavigate } from 'react-router-dom';
+import {
+Table,
+TableBody,
+TableContainer,
+TableCell,
+TableHead,
+TableRow,
+Paper,
+Button,
+Typography
 
 
+} from '@mui/material'
+import { EditRounded, DeleteOutlined } from '@mui/icons-material'
 const List = () => {
-    const navigate=useNavigate()
+    const navigate = useNavigate()
     const [getList, setList] = useState([])
-    
+
     const [error, setError] = useState(null);
 
     const apiResp = async () => {
@@ -21,10 +33,10 @@ const List = () => {
     }, [])
     console.log("getlist", getList)
 
-    
+
     const updateItem = async (id) => {
         try {
-           
+
             navigate(`/add/${id}`)
         } catch (error) {
             console.error("Error updating item:", error);
@@ -41,53 +53,42 @@ const List = () => {
     }
     return (
         <div>
-            <h1> Notes Data</h1>
+            <h1 style={{ fontFamily: 'Arial, Helvetica, sans-serif' }}> Notes Data</h1>
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <table   style={{marginTop:"20px"}}>
-                    <tr >
-                        <th >Title</th>
-                        <th>Description</th>
-                        <th>Actions</th>
 
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell style={{ fontWeight: "bold" }}>Title</TableCell>
+                                <TableCell style={{ fontWeight: "bold" }}>Description</TableCell>
+                                <TableCell style={{ fontWeight: "bold" }}>Update</TableCell>
+                                <TableCell style={{ fontWeight: "bold" }}>Delete</TableCell>
 
-                    </tr>
-                    {getList?.map((item) => (
-                        <>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {getList?.map((item) => (
+                                <TableRow
+                                    key={item._id}
+                                >
+                                    <TableCell >
+                                        {item.title}
+                                    </TableCell>
+                                    <TableCell >{item.comment}</TableCell>
 
-                            <tr key={item._id} >
-                                <td >{item?.title}</td>
-                                <td>{item?.comment}</td>
-                                <button onClick={() => updateItem(item._id)} style={{
-                                    backgroundColor:'#99ff99',                               
-                                    
-                                padding:"5px",
-                                marginLeft:'2px',
-                                
-                               
-                                }}>
-                                    Update
-                                </button>
-                                
-                                <button onClick={() => deleteNote(item._id)} 
-                                style={{backgroundColor:' #ff8080',
-                                // color:'white',
-                                padding:"5px",
-                                marginLeft:'5px',
-                               
-                            }}
-                                >Delete</button>
+                                    <TableCell > <EditRounded onClick={() => updateItem(item._id)} style={{ color: '#668cff' }} /> </TableCell>
+                                    <TableCell> <DeleteOutlined onClick={() => deleteNote(item._id)} style={{ color: '#ff8080' }} /></TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
 
-                            </tr>
-
-
-                        </>
-                    ))}
-                </table>
-                
             </div>
-            
+
         </div>
-        
+
     )
 }
 
